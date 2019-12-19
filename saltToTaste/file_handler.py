b655d21d9fdc3a2c05b4dcf9cf8df7ab33615923
TheMeanCanEHdian <drivard100@gmail.com>
2019-12-19 13:29:56 -0600

    Test switch from print to logging

--- saltToTaste/file_handler.py
+++ saltToTaste/file_handler.py
@@ -1,8 +1,9 @@
 import os
+import logging
 
 def create_flask_secret(DATA_DIR):
     # if not os.path.isfile('saltToTaste.secret'):
-    print (f' * Creating Flask secret', flush=True)
+    logging.info(f' * Creating Flask secret')
     f = open(f'{DATA_DIR}/saltToTaste.secret', 'w+', encoding='utf-16')
     secret = os.urandom(24).hex()
     f.write(f'{secret}')
